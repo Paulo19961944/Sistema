5d67d08ab8d8de17a9b667bd6a8e442b453c738f
Paulo19961944 <106405308+Paulo19961944@users.noreply.github.com>
2023-08-05 17:22:35 -0700

    Update Nota.java

--- src/com/sp/senai/nota/Nota.java
+++ src/com/sp/senai/nota/Nota.java
@@ -1,39 +1,50 @@
 package com.sp.senai.nota;
 import java.util.Scanner;
+
 public class Nota {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
+        /** Transforma em Loop Infinito */
+        boolean form = true;
         /** Validação */
-        System.out.print("Digite o Curso: ");
-        String curso = teclado.next();
+        while (form) {
+            System.out.print("Digite o Curso: ");
+            String curso = teclado.next();
 
-        System.out.print("Digite o nome do Professor: ");
-        String nomeProf = teclado.next();
+            System.out.print("Digite o nome do Professor: ");
+            String nomeProf = teclado.next();
 
-        System.out.print("Digite a Quantidade de Alunos");
-        int qtdeAlunos = teclado.nextInt();
+            /** Verifica a Frequencia */
+            System.out.print("Quantos dias o aluno cursou?: ");
+            float diasCursados = teclado.nextFloat();
+            float diasTotais = 100;
 
-        /** Verifica a Frequencia */
+            /** Verifica a nota */
+            System.out.print("Qual é a Primeira Nota: ");
+            float nota1 = teclado.nextFloat();
 
-        System.out.print("Quantos dias o aluno cursou?: ");
-        int diasCursados = teclado.nextInt();
-        int diasTotais = 100;
+            System.out.print("Qual a Segunda Nota: ");
+            float nota2 = teclado.nextFloat();
 
-        /** Verifica a nota */
-        System.out.print("Qual é a Primeira Nota: ");
-        float nota1 = teclado.nextFloat();
-
-        System.out.print("Qual a Segunda Nota:");
-        float nota2 = teclado.nextFloat();
-
-        if ((diasCursados >= 75 && (nota1 + nota2 / 2 >= 7) || (diasCursados >= 90) && (nota1 + nota2 /2 >= 6))){
+            if ((diasCursados >= 75 && (nota1 + nota2) / 2 >= 7) || (diasCursados >= 90 && (nota1 + nota2) / 2 >= 6)) {
                 System.out.println("Aprovado");
-            }
-            else {
+            } else {
                 System.out.println("Reprovado");
             }
+            /** Calcula a Porcentagem de Dias Cursados */
+            float porcentagem = (diasCursados / diasTotais) * 100;
+            float mediaNota = (nota1 + nota2) / 2;
+            System.out.println();
+            System.out.println("A porcentagem de Presença e: " + porcentagem + "%");
+            System.out.println("A media da nota é: " + mediaNota);
+
+            /** Verifica se quer continuar ou não */
+            System.out.println("Caso deseje encerrar digite N:, Caso deseje continuar, digite qualquer outra letra: ");
+            String opcao = teclado.next();
+            if (opcao.equalsIgnoreCase("N")) {
+                form = false;
+            }
         }
-
     }
-
+}
